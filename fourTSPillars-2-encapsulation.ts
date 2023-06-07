987d578fd8f2f61398a2b926c3e2101aa866cea4
Olga Bouin <olga.bouin@adeo.com>
2023-06-07 20:01:10 +0200

    feat: add encapsulation

--- fourTSPillars-2-encapsulation.ts
+++ fourTSPillars-2-encapsulation.ts
@@ -1,7 +1,50 @@
-//////////////////////////1 Heritage
-
 //////////////////////////2 Encapsulation
 
-//////////////////////////3 Abstraction
+class Enemy2 {
+    name: string
+    phrase: string
+    power: number
+    speed: number
+    constructor (name: string, phrase: string, power: number, speed: number) {
+        this.name = name
+        this.phrase = phrase
+        this.power = power
+        this.speed = speed
+    }
+    move = () => console.log(`I'm moving at the speed of ${this.speed}!`)
+    attack = () => console.log(`I'm attacking with a power of ${this.power}!`)
+    sayPhrase = () => console.log(this.phrase)
+}
 
-//////////////////////////4 Polymorphism
+class Alien2 extends Enemy2 {
+    species: string
+    #birthYear: number
+    constructor (name: string, phrase: string, power: number, speed: number, birthYear: number) {
+        super(name, phrase, power, speed)
+        this.species = "alien"
+        this.#birthYear = birthYear
+    }
+    fly = () => console.log("Zzzzzziiiiiinnnnnggggg!!")
+    howOld = () => console.log(`I was born in ${this.#birthYear}`)
+}
+
+const alien = new Alien2("Ali", "I'm Ali the alien!", 10, 50, 2000)
+
+console.log(alien.name) // вывод: Ali
+alien.sayPhrase() // вывод: "I'm Ali the alien!"
+alien.howOld() // вывод: "I was born in 2000"
+//console.log(alien1.#birthYear) // Erreur !
+console.log(alien)
+// Alien2 {
+//     move: [Function (anonymous)],
+//         attack: [Function (anonymous)],
+//         sayPhrase: [Function (anonymous)],
+//         name: 'Ali',
+//         phrase: "I'm Ali the alien!",
+//         power: 10,
+//         speed: 50,
+//         fly: [Function (anonymous)],
+//         howOld: [Function (anonymous)],
+//         species: 'alien'
+// } // birthYear n'est pas affiché !
+
